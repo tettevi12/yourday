eed28b0f3c61960910a7a63224150f8ed5c3bd6c
Capt <selormacat@gmail.com>
2025-04-15 22:12:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-
 async function fetchWikipediaEvents(month, day) {
   const url = `https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/${month}/${day}`;
   const response = await fetch(url);
   const data = await response.json();
   return data.events;
 }
+
 async function generateTimeline() {
   const date = document.getElementById("birthdate").value;
   if (!date) return alert("Please enter a date.");
@@ -15,14 +15,28 @@ async function generateTimeline() {
 
   const events = await fetchWikipediaEvents(parseInt(month), parseInt(day));
 
-  events.slice(0, 5).forEach(event => {
+  events.slice(0, 6).forEach(event => {
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
-      <h3>Year: ${event.year}</h3>
-      <p>${event.text}</p>
+      <h3>${event.year}</h3>
+      <h4>${event.text}</h4>
+      <a href="https://en.wikipedia.org/wiki/${event.pages[0].normalizedtitle}" target="_blank">Read more</a>
     `;
     results.appendChild(card);
   });
+
+  addShareButtons();
 }
 
+function addShareButtons() {
+  const results = document.getElementById("results");
+  const shareDiv = document.createElement("div");
+  shareDiv.className = "share-buttons";
+  shareDiv.innerHTML = `
+    <h4>Share your day:</h4>
+    <a href="https://twitter.com/intent/tweet?text=Look what happened on my birthday!&url=https://yourusername.github.io/yourday-explorer" target="_blank">Share on Twitter</a>
+    <a href="https://www.facebook.com/sharer/sharer.php?u=https://yourusername.github.io/yourday-explorer" target="_blank">Share on Facebook</a>
+  `;
+  results.appendChild(shareDiv);
+}
