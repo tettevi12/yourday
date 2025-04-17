086d65ac84867c97db44e352d5b3e9630c391d30
Capt <selormacat@gmail.com>
2025-04-16 22:59:10 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   NEWS_API_KEY: "064d1e3c" + "8c1e4daa" + "8e44414e977a5ba2",
-  GOOGLE_API_KEY: "AIzaSyAoPQ1syerxiUUf4ww_9qCsA29i7FEn3-s",
+  GOOGLE_API_KEY: ["AIzaSyAo", "PQ1syerxiUU", "f4ww_9qCsA29i7FEn3-s"].join(""),
   GOOGLE_CX: "61b1a668b139d441c"
 };
